28f005b0de6ac7e18aeec761ae5d82c84bbc5b89
Rampal Chopra <rampal.chopra@ticketmaster.com>
2015-11-09 16:48:10 -0500

    subscriber clients will find cluster members to connect to nats

--- subscriber.go
+++ subscriber.go
@@ -1,6 +1,11 @@
 package vindalu
 
 import (
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+
 	log "github.com/euforia/simplelog"
 	"github.com/nats-io/nats"
 
@@ -12,11 +17,14 @@ type VindaluSubscriber struct {
 	enConn *nats.EncodedConn
 }
 
-func NewVindaluSubscriber(servers []string, logger *log.Logger) (vs *VindaluSubscriber, err error) {
+func NewVindaluSubscriber(server string, logger *log.Logger) (vs *VindaluSubscriber, err error) {
 	vs = &VindaluSubscriber{}
 
 	opts := nats.DefaultOptions
-	opts.Servers = servers
+	opts.Servers, err = GetNatsServers(server)
+	if err != nil {
+		return
+	}
 
 	if vs.conn, err = opts.Connect(); err != nil {
 		return
@@ -33,6 +41,28 @@ func NewVindaluSubscriber(servers []string, logger *log.Logger) (vs *VindaluSubs
 
 	vs.enConn, err = nats.NewEncodedConn(vs.conn, nats.JSON_ENCODER)
 
+	return
+}
+
+func GetNatsServers(server string) (natsServers []string, err error) {
+	var (
+		clusterStatus *core.VindaluClusterStatus
+		resp *http.Response
+		body []byte
+	)
+	resp, err = http.Get(server + "/status")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(body, &clusterStatus)
+	if err != nil {
+		return
+	}
+	for _, member := range clusterStatus.ClusterMemberAddrs() {
+		natsServers = append(natsServers, fmt.Sprintf("nats://%s:%d", member, 4223))
+	}
 	return
 }
 
